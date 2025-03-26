9fd22db6d265812998144bdf0eb86a9fd1794678
史安朔 <shianshuo@shianshuodeMacBook-Pro-2.local>
2025-03-26 10:30:43 +0800

    add search

--- src/ConnectEchooo.tsx
+++ src/ConnectEchooo.tsx
@@ -4,17 +4,23 @@ const ConnectEchooo: React.FC = () => {
     const handleConnect = () => {
         const userAgent = navigator.userAgent.toLowerCase();
 
+        const queryParams = window.location.search; 
+        const baseAndroidUrl = 'echooo://echooo.valleysound.xyz/vss/ton-connect';
+        const finalAndroidUrl = `${baseAndroidUrl}${queryParams}`; 
+
+        const baseIOSUrl = 'https://api.valleysound.xyz/vss';
+        const finalIOSUrl = `${baseIOSUrl}${queryParams}`; // 拼接参数
         // 判断是否是 Android 设备
         if (userAgent.includes('android')) {
-            window.open('echooo://echooo.valleysound.xyz/vss/ton-connect', '_blank');
+            window.open(finalAndroidUrl, '_blank');
         } 
         
         else if (userAgent.includes('iphone') || userAgent.includes('ipad') || userAgent.includes('ipod')) {
-            window.open('https://api.valleysound.xyz/vss', '_blank');
+            window.open(finalIOSUrl, '_blank');
         } 
         // 默认情况
         else {
-            window.open('https://api.valleysound.xyz/vss', '_blank');
+            window.open(finalIOSUrl, '_blank');
         }
     };
 
