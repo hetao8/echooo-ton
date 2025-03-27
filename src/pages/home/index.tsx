19fa90f84156b2f8ce3f7d900e0d4bb7a2c7a2d6
史安朔 <shianshuo@shianshuodeMacBook-Pro-2.local>
2025-03-27 12:25:59 +0800

    echooo-ton

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,9 @@
-import  { useState } from 'react';
 import ConnectEchooo from '../../ConnectEchooo';
 
 const Home = () => {
-    const [showConnect, setShowConnect] = useState(false);
-
-    const handleShowConnect = () => {
-        setShowConnect(true);
-    };
-
     return (
         <div>
-            <button onClick={handleShowConnect}>连接echooo钱包</button>
-            {showConnect && <ConnectEchooo />}
+            <ConnectEchooo />
         </div>
     );
 };
